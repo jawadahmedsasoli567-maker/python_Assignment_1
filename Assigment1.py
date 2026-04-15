b237ed2208d262675ff125a3c9da7e67393f6693
jawadahmedsasoli567-maker <jawadahmedsasoli567@gmail.com>
2026-04-15 14:19:23 +0500

    Add files via upload

---
+++ Assigment1.py
@@ -0,0 +1,29 @@
+print("Welcome to Python")
+print("Welcome to Computer Science")
+print("Programming is fun")
+
+
+print("Welcome to Python")
+print("Welcome to Python")
+print("Welcome to Python")
+print("Welcome to Python")
+print("Welcome to Python")
+
+a=1
+b=2
+c=3
+d=4
+e=5
+f=6
+g=7
+h=8
+i=9
+y=a+b+c+d+e+f+g+h+i
+print(y)
+
+radius=5.5
+PI=3.14
+perimeter=2*radius*PI;
+area= radius*radius*PI;
+print("Perimeter of curcle", perimeter)
+print("Area of curcle", area)
